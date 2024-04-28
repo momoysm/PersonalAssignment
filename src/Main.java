2e23af0da0036a5a83b307e4d9856a04cc4ab829
seongmo-yoon <momoysm@gmail.com>
2024-04-28 14:46:18 +0900

    1.7 commit

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,22 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] result = new int[10]; //결과값
-        int count = 0;
+        ArrayList<Integer> resultList = new ArrayList<Integer>(); //결과값 저장 List
+        int result = 0; //결과값
         String error = ""; //에러시 출력 문장
         String exit = ""; //계산 종료 input
+        String remove = ""; //결과값 배열 첫번째 값 제거 명령어?
 
         int input1 = 0; //첫 번째 입력값
         int input2 = 0; //두 번째 입력값
         char operator = ' '; //연산자 입력값
 
+        //exit입력시 반복 종료
         while(!exit.equals("exit")){
-
-            if(count == 10){
-                for(int i=0; i<10; i++){
-                    if(i < 9){
-                        result[i] = result[i+1];
-                    }else{
-                        result[i] = 0;
-                    }
-                    System.out.println(i + " : " + result[i]);
-                }
-
-                count--;
-            }
 
             System.out.print("첫 번째 숫자를 입력하세요 : ");
             input1 = Integer.parseInt(br.readLine());
@@ -39,19 +29,19 @@ public class Main {
 
             switch (operator) {
                 case('+'):
-                    result[count] = input1 + input2;
+                    result = input1 + input2;
                     break;
                 case('-'):
-                    result[count] = input1 - input2;
+                    result = input1 - input2;
                     break;
                 case('*'):
-                    result[count] = input1 * input2;
+                    result = input1 * input2;
                     break;
                 case('/'):
                     if(input2 == 0) {
                         error = "0으로는 나눌 수 없습니다.";
                     }else{
-                        result[count] = input1 / input2;
+                        result = input1 / input2;
                     }
                     break;
                 default:
@@ -60,11 +50,20 @@ public class Main {
             }
 
             if(error.isEmpty()){
-                System.out.println("결과 : " + result[count]); //error가 없을 때 결과값 출력
-                count++;
+                System.out.println("결과 : " + result); //error가 없을 때 결과값 출력
+                resultList.add(result);
             }else{
                 System.out.println(error); //error가 있으면 error출력
             }
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            remove = br.readLine();
+
+            //remove입력시 결과값 리스트에서 첫번째 값 제거
+            if(remove.equals("remove")){
+                resultList.remove(0);
+            }
+
             System.out.println("enter any word to continue? (exit 입력 시 종료)");
             exit = br.readLine();
         }
